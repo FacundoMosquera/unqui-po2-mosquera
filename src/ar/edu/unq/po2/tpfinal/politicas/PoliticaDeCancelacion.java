c0940c609218301009942d1bfa5e17fad54649e6
FacundoMosquera <112212862+FacundoMosquera@users.noreply.github.com>
2025-04-16 21:47:18 -0300

    Delete src/ar/edu/unq/po2 directory

--- src/ar/edu/unq/po2/tpfinal/politicas/PoliticaDeCancelacion.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unq.po2.tpfinal.politicas;
-import ar.edu.unq.po2.tpfinal.inmueble.Inmueble;
-
-
-public abstract class PoliticaDeCancelacion {
-	
-	public abstract int getResarcimiento(Inmueble i);
-}
-
--- src/ar/edu/unq/po2/tpfinal/reserva/EstadoReserva.java
+++
@@ -1,7 +0,0 @@
-package ar.edu.unq.po2.tpfinal.reserva;
-
-public abstract class EstadoReserva {
-	
-	 
-	
-}
