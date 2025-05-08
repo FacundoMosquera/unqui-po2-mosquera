23dd596254c78d754fb7ae1e13dbaa73a26999b0
leanefe <l.fabeiro07@gmail.com>
2025-05-08 16:32:12 -0300

    clase cuadrado, extension de rectangulo y test de tp4

---
+++ src/ar/edu/unq/po2/tp3/Cuadrado.java
@@ -0,0 +1,10 @@
+package ar.edu.unq.po2.tp3;
+
+public class Cuadrado extends Rectangulo {
+
+	private int lado;
+	
+	
+	@Override
+	public Cuadrado()
+}
--- src/ar/edu/unq/po2/tp3/Rectangulo.java
+++ src/ar/edu/unq/po2/tp3/Rectangulo.java
@@ -2,6 +2,9 @@ package ar.edu.unq.po2.tp3;
 
 public class Rectangulo {
 	
+	int altura;
+	int base;
+	
 	Point EsquinaSuperiorIzq;
 	Point EsquinaSuperiorDer;
 	Point EsquinaInferiorIzq;
@@ -13,5 +16,20 @@ public class Rectangulo {
 		this.EsquinaSuperiorDer = ubicacion.sumarPunto(new Point(base, 0));
 		this.EsquinaInferiorIzq = ubicacion.sumarPunto(new Point(0, -altura));
 		this.EsquinaInferiorDer = ubicacion.sumarPunto(new Point(base, -altura));
+		
+		this.base = base;
+		this.altura = altura;
+	}
+	
+	public int calcularArea() {
+		return base * altura; 
+	}
+	
+	public int calcularPerimetro() {
+		return 2*(base + altura);
+	}
+	
+	public boolean esHorizontal() {
+		return base > altura;
 	}
 }
